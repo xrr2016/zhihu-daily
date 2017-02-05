abf2fb6fab6995c9cca11c25265d383967ca0c86
xrr2016 <13286904669@163.com>
2017-02-06 07:36:32 +0800

    :fire: add font icon css

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,54 @@
 <template>
   <div class="navbar">
+    <!-- 导航栏 -->
      <md-toolbar>
        <md-button class="md-icon-button" @click="toggleSideNav">
-         <md-icon class="md-primary">menu</md-icon>
+         <md-icon>menu</md-icon>
        </md-button>
        <h2 class="md-title" style="flex: 1">知乎日报</h2>
-       <md-menu md-align-trigger>
-        <md-button md-menu-trigger>Align trigger</md-button>
-          <md-menu-content>
-            <md-menu-item>My Item 1</md-menu-item>
-            <md-menu-item>My Item 2</md-menu-item>
-            <md-menu-item>My Item 3</md-menu-item>
-          </md-menu-content>
-        </md-menu>
        <p>{{msg}}</p>
     </md-toolbar>
-    <!-- sidenav -->
+    <!-- 侧边栏 -->
     <md-sidenav class="md-left" ref="leftSidenav">
       <md-list>
-       <md-list-item @click="closeSideNav">
+       <md-list-item>
          <router-link to="/">首页</router-link>
        </md-list-item>
-       <md-list-item @click="closeSideNav">
-         <router-link to="/story">主题</router-link>
+       <md-list-item>
+         <router-link to="/psychology">日常心理学</router-link>
+       </md-list-item>
+       <md-list-item>
+         <router-link to="/recommend">用户推荐日报</router-link>
+       </md-list-item>
+       <md-list-item>
+         <router-link to="/movie">电影日报</router-link>
+       </md-list-item>
+       <md-list-item>
+         <router-link to="/notBoring">不许无聊</router-link>
+       </md-list-item>
+       <md-list-item>
+         <router-link to="/design">设计日报</router-link>
+       </md-list-item>
+       <md-list-item>
+         <router-link to="/bigCompany">大公司日报</router-link>
+       </md-list-item>
+       <md-list-item>
+         <router-link to="/finance">财经日报</router-link>
+       </md-list-item>
+       <md-list-item>
+         <router-link to="/internet">互联网安全</router-link>
+       </md-list-item>
+       <md-list-item>
+         <router-link to="/game">开始游戏</router-link>
+       </md-list-item>
+       <md-list-item>
+         <router-link to="/music">音乐日报</router-link>
+       </md-list-item>
+       <md-list-item>
+         <router-link to="/anime">动漫日报</router-link>
+       </md-list-item>
+       <md-list-item>
+         <router-link to="/sports">体育日报</router-link>
        </md-list-item>
    </md-sidenav>
   </div>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
+import './assets/material.font.css'
 
 Vue.use(VueMaterial)
 
